3b6c5eccf7b9dba24549caccf4ee080e1d917dc7
Cherry <lenyamm35@gmail.com>
2022-09-29 00:59:41 +0300

    authorization complete

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -20,7 +20,7 @@ class UserController {
     async login(req, res, next) {
         try {
             const {email, password} = req.body
-            const userData = await userService.login({email, password})
+            const userData = await userService.login(email, password)
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
             return res.json(userData)
         } catch(e) {
@@ -29,7 +29,10 @@ class UserController {
     } 
     async logout(req, res, next) {
         try {
-
+            const {refreshToken} = req.cookies
+            const token = await userService.logout(refreshToken)
+            res.clearCookie('refreshToken')
+            return res.json(token)
         } catch(e) {
             next(e)
         }
@@ -45,14 +48,18 @@ class UserController {
     } 
     async refresh (req, res, next) {
         try {
-
+            const {refreshToken} = req.cookies
+            const userData = await userService.refresh(refreshToken)
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            return res.json(userData)
         } catch(e) {
             next(e)
         }
     } 
     async getUsers (req, res, next) {
         try {
-            res.json(['234', '242'])
+            const users = await userService.getAllUsers()
+            res.json(users)
         } catch(e) {
             next(e)
         }
--- service/token-service.js
+++ service/token-service.js
@@ -9,6 +9,22 @@ class TokenService {
             refreshToken
         }
     }
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+            return userData
+        } catch (e) {
+            return null
+        }
+    }
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+            return userData
+        } catch (e) {
+            return null
+        }
+    }
     async saveToken(userId,refreshToken) {
         const tokenData = await tokenModel.findOne({user: userId})
         if (tokenData) {
@@ -17,6 +33,14 @@ class TokenService {
         }
         const token = await tokenModel.create({user: userId, refreshToken})
         return token
+    }
+    async removeToken(refreshToken) {
+        const tokenData = await tokenModel.deleteOne({refreshToken})
+        return tokenData
+    }
+    async findToken(refreshToken) {
+        const tokenData = await tokenModel.findOne({refreshToken})
+        return tokenData
     }
 }   
 
