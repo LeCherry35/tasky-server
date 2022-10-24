ea1f6a508e179cd34ca51dfdcdecb86ca80cc364
Cherry <lenyamm35@gmail.com>
2022-10-24 13:13:52 +0300

    withCredentials false

--- index.js
+++ index.js
@@ -11,15 +11,18 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+// app.use(cors({
+//     credentials: true,
+//     origin: process.env.CLIENT_URL
+// }))
 app.use(cors({
-    credentials: true,
     origin: process.env.CLIENT_URL
 }))
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 app.use('/api', router)
 app.use(errorMiddleware)
 
