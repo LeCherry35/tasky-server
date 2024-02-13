33df7bb8fdde5bc31f69f2cc77340800556298eb
Cherry <lenyamm35@gmail.com>
2024-02-13 11:35:35 +0200

    mail service is temporary unavaliable

--- service/user-service.js
+++ service/user-service.js
@@ -15,7 +15,9 @@ class UserService {
         const hashPassword = await bcrypt.hash(password, 3)
         const activationLink = uuid.v4()
         const user = await UserModel.create({email, password: hashPassword, activationLink})
-        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`)
+        // console.log('beforeMail');
+        // await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`)
+        // console.log('afterMail');
         const userDto = new UserDto(user)
         const tokens = tokenService.generateTokens({...userDto})
         await tokenService.saveToken(userDto.id, tokens.refreshToken)
