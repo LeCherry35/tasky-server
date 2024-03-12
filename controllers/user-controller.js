b6152c18c701b1a8b644f041ced93a2cdcf04cfb
Cherry <lenyamm35@gmail.com>
2024-03-13 00:51:02 +0200

    cookie fix secure: true

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -21,7 +21,7 @@ class UserController {
         try {
             const {email, password} = req.body
             const userData = await userService.login(email, password)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none'})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none', secure: true})
             return res.json(userData)
         } catch(e) {
             next(e)
