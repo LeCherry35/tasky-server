ec7b4fa2a41f06ff0fffaf7f1028cc034baa39fc
Cherry <lenyamm35@gmail.com>
2024-03-13 00:58:45 +0200

    refresh and registration cookie fix

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -11,7 +11,7 @@ class UserController {
             }
             const {email, password} = req.body
             const userData = await userService.registration(email, password)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none', secure: true})
             return res.json(userData)
         } catch(e) {
             next(e)
@@ -50,7 +50,7 @@ class UserController {
         try {
             const {refreshToken} = req.cookies
             const userData = await userService.refresh(refreshToken)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none', secure: true})
             return res.json(userData)
         } catch(e) {
             next(e)
